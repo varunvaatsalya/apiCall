bed17a913088351502fde4b9c583aa883a78c69e
varunvaatsalya <a.varungupta23@gmail.com>
2024-09-27 23:03:08 +0530

    error

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-function page() {
+function Home() {
   const [data, setData] = useState(null);
   const [resData, setResData] = useState(null);
 
@@ -99,4 +99,4 @@ function page() {
   );
 }
 
-export default page;
+export default Home;
